cc25a38f8172576fee62fb359bfaa6e9e27ac4c8
HankB-o-t <t37.slime.1.1@gmail.com>
2024-11-05 19:20:04 -0300

    comp

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use std::process;
-use std::thread;
-use std::time;
-// array:
-//   c    w   h
-// [[""; 4]; 6]
-// a[y][x]
-
-fn main() {
-    let w = 30; let h = 10;
-    let mut arr = vec![vec!["_"; w]; h];
-    let ms10 = time::Duration::from_millis(250);
-    let mut now = time::Instant::now();
-
-    loop {
-        let e = now.elapsed();
-        //print
-        for i in 0..h {
-            for j in 0..w {
-                arr[i][j] = ".";
-                arr[i][i*2] = "o";
-                arr[i][i*3] = "a";
-                arr[i][i/2] = "#";
-                arr[i][i/4] = "[";
-                arr[i][i] = "@";
-                print!("{}", arr[i][j]);
-            }
-            println!("");
-        }
-        println!("{}",e.as_secs());
-        thread::sleep(ms10);
-        process::Command::new("clear").status().unwrap();
-    }
-}
+use std::{process,thread,time};fn main(){let w=30;let h=
+10;let mut arr =vec![vec!["_";w];h];let ms10=time::Duration
+::from_millis(250);let mut now=time::Instant::now();let mut
+r=0;let mut v=0;let mut g=0;loop {let e =now.elapsed();for i
+in 0..h {for j in 0..w{arr[i][j] =" ";arr[i][r] ="|";arr[v]
+[j]= "-";arr[v][r] ="+";arr[i][i*g] = "o";print!("{}",arr[i]
+[j]);}println!("");}if g>=3{g=0}g+=1;if r>=w-1{r=0}else{r+=1}
+if v>=h-1{v=0}else{v+=1}println!("x: {} y: {}",r,v);println!(
+"s: {}",e.as_secs());thread::sleep(ms10);process::Command
+::new("clear").status().unwrap();}}
